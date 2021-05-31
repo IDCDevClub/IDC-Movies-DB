4f3f444bdf901fea62c791d0946b5b363eb61c9b
IDC Developers Club <84461196+IDCDevClub@users.noreply.github.com>
2021-05-31 17:17:46 +0300

    Add files via upload

---
+++ fullStack_project_devclub/api/api.py
@@ -0,0 +1,18 @@
+import json
+from flask import Flask, request
+from db import SQLiteConnection
+
+app = Flask(__name__)
+sqlite_connection = SQLiteConnection()
+
+
+
+def add_to_favorites():
+    # TODO
+   
+
+
+
+def get_favorites():
+    # TODO
+    
---
+++ fullStack_project_devclub/api/db.py
@@ -0,0 +1,41 @@
+import sqlite3
+from typing import List, Dict
+
+DB_NAME = "movies.db"
+
+
+class SQLiteConnection:
+    def __init__(self):
+        self._connection = None
+        self.init_favorites_table()
+
+    def execute_command(self, command, get=False):
+        fetchall = None
+        try:
+            self._connection = sqlite3.connect(DB_NAME)
+            cursor = self._connection.cursor()
+            print("Connected to SQLite")
+
+            cursor.execute(command)
+            if get:
+                fetchall = cursor.fetchall()
+            print("executed")
+            self._connection.commit()
+            cursor.close()
+        except sqlite3.Error as error:
+            print(error)
+        finally:
+            if self._connection:
+                self._connection.close()
+                print("The SQLite connection is closed")
+        return fetchall
+
+    def init_favorites_table(self):
+        command = """CREATE TABLE if not exists add_to_favorites (
+        id INTEGER PRIMARY KEY,
+        imdbID INTEGER,
+        title VARCHAR(20)
+        );"""
+        self.execute_command(command)
+
+# TODO
