633bf6ff50ce70a5eb0821e4f82c4b29ac2b66e6
IDC Developers Club <84461196+IDCDevClub@users.noreply.github.com>
2021-05-31 17:24:21 +0300

    Add files via upload

---
+++ fullStack_project_devclub/react-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+// import Movies from "./Components/Movies";
+// import MoviesHeading from "./Components/MoviesHeading";
+// import SearchBox from "./Components/SearchBox";
+// import AddToFavorites from "./Components/AddToFavorites";
+
+const API_KEY = "598d1c39"; // to OMDB
+// const API_KEY = "338d9da5ffde4fe6044c0fd2c82d4457"; // to TMDB
+
+const App = () => {};
+
+export default App;
