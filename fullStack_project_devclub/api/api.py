44cc3c359882660e31892bb53fce7db58b549fec
IDC Developers Club <84461196+IDCDevClub@users.noreply.github.com>
2021-06-07 20:37:56 +0300

    Update api.py

--- fullStack_project_devclub/api/api.py
+++ fullStack_project_devclub/api/api.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 sqlite_connection = SQLiteConnection()
 
 
-
+@app.route('/api/add_to_favorites', methods=['POST'])
 def add_to_favorites():
-    # TODO
-   
+    data = json.loads(request.data)
+    sqlite_connection.save_to_favorites(data)
+    return {}
 
 
-
+@app.route('/get_favorites', methods=['GET'])
 def get_favorites():
-    # TODO
-    
+    return json.dumps(sqlite_connection.get_list_of_favorites())
